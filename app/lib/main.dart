903e0db65b08b6f6e3218efc75b1fe3f86873900
yama-koo <yamako.ilc@gmail.com>
2019-12-01 18:03:09 +0900

    interactivity

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter/foundation.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,171 +8,161 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter layout demo',
-      home: buildHomePage("真っ白なキャンパス"),
-    );
-  }
-
-  Widget _buildList() {
-    var tiles = new List<Widget>();
-    for (var i = 0; i < 6; i++) {
-      tiles.add(_tile('西野千明', '真っ白なキャンパス', Icons.theaters));
-      tiles.add(_tile('小野寺梓', '真っ白なキャンパス', Icons.theaters));
-      tiles.add(_tile('麦田ひかる', '真っ白なキャンパス', Icons.theaters));
-      tiles.add(_tile('橋本美桜', '真っ白なキャンパス', Icons.theaters));
-      tiles.add(_tile('三浦菜々子', '真っ白なキャンパス', Icons.theaters));
-      tiles.add(_tile('鈴木えま', '真っ白なキャンパス', Icons.theaters));
-    }
-    return ListView(
-      children: tiles,
-    );
-  }
-
-  Dismissible _tile(String title, String subTitle, IconData icon) =>
-      Dismissible(
-        child: ListTile(
-          title: Text(
-            title,
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 20,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('真っ白なキャンパス'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image(
+              image:
+                  NetworkImage('http://shirokyan.com/images/fix_rgb_wht.png'),
             ),
-          ),
-          subtitle: Text(subTitle),
-          leading: Icon(
-            icon,
-            color: Colors.blue[500],
-          ),
-        ),
-        background: Container(
-          color: Colors.red,
-        ),
-        secondaryBackground: Container(
-          color: Colors.green,
-        ),
-        key: UniqueKey(),
-      );
-
-  Widget _logo() => Image(
-        image: NetworkImage('http://shirokyan.com/images/fix_rgb_wht.png'),
-        fit: BoxFit.contain,
-      );
-
-  Widget buildHomePage(String title) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              LimitedBox(
-                maxHeight: 400,
-                child: _buildList(),
-              ),
-              Divider(
-                thickness: 1,
-              ),
-              _logo(),
-            ],
-          ),
+            // Spacer(),
+            Container(
+              child: FavoriteWidget(),
+            ),
+            Container(
+              child: TapboxA(),
+            ),
+            Container(
+              child: ParentWidget(),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
 
-  @override
+  // @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.list),
-            onPressed: _pushSaved,
-          )
-        ],
-      ),
-      body: _buildSuggestions(),
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red,
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Container(
+              child: Text('$_favoriteCount'),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /* 1 */ (context, i) {
-          if (i.isOdd) return Divider(); /* 2 */
-
-          final index = i ~/ 2; /* 3 */
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /* 4 */
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
+  void _toggleFavorite() {
+    setState(
+      () {
+        if (_isFavorited) {
+          _favoriteCount -= 1;
+          _isFavorited = false;
+        } else {
+          _favoriteCount += 1;
+          _isFavorited = true;
+        }
       },
     );
   }
+}
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(
-              children: divided,
-            ),
-          );
-        },
+class FavoriteWidget extends StatefulWidget {
+  // @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class ParentWidget extends StatefulWidget {
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxB(active: _active, onChanged: _handleTapboxChanged),
+    );
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? "Active" : "Inactive",
+            style: TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+        width: 200,
+        height: 200,
+        decoration:
+            BoxDecoration(color: active ? Colors.lightGreen : Colors.grey),
       ),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+        width: 200,
+        height: 200,
+        decoration:
+            BoxDecoration(color: _active ? Colors.lightGreen : Colors.grey),
+      ),
+    );
+  }
+}
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+  _TapboxAState createState() => _TapboxAState();
 }
