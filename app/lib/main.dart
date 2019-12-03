b75404ac5f2c83ec71384d5657aea00cd4307589
yama-koo <yamako.ilc@gmail.com>
2019-12-04 01:12:07 +0900

    bottom bar

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final List listItem = ['西野千明', '小野寺梓', '橋本美桜', '麦田ひかる', '三浦菜々子', '鈴木えま'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,157 +12,50 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('真っ白なキャンパス'),
         ),
-        body: Column(
-          children: <Widget>[
-            Image(
-              image:
-                  NetworkImage('http://shirokyan.com/images/fix_rgb_wht.png'),
-            ),
-            // Spacer(),
-            Container(
-              child: FavoriteWidget(),
-            ),
-            Container(
-              child: TapboxA(),
-            ),
-            Container(
-              child: ParentWidget(),
-            ),
-          ],
+        body: Container(
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text(listItem[index % 6]),
+                key: UniqueKey(),
+              );
+            },
+            itemCount: 200,
+          ),
         ),
+        bottomNavigationBar: BottomBar(),
       ),
     );
   }
 }
 
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = true;
-  int _favoriteCount = 41;
+class BottomBar extends StatefulWidget {
+  _BottomBarState createState() => _BottomBarState();
+}
 
-  // @override
+class _BottomBarState extends State<BottomBar> {
+  int currentIndex = 0;
+
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(0),
-          child: IconButton(
-            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
-            color: Colors.red,
-            onPressed: _toggleFavorite,
-          ),
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          title: Text('home'),
         ),
-        SizedBox(
-          width: 18,
-          child: Container(
-            child: Container(
-              child: Text('$_favoriteCount'),
-            ),
-          ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          title: Text('setting'),
         ),
       ],
+      currentIndex: currentIndex,
+      onTap: onTap,
     );
   }
-
-  void _toggleFavorite() {
-    setState(
-      () {
-        if (_isFavorited) {
-          _favoriteCount -= 1;
-          _isFavorited = false;
-        } else {
-          _favoriteCount += 1;
-          _isFavorited = true;
-        }
-      },
-    );
-  }
-}
-
-class FavoriteWidget extends StatefulWidget {
-  // @override
-  _FavoriteWidgetState createState() => _FavoriteWidgetState();
-}
-
-class ParentWidget extends StatefulWidget {
-  _ParentWidgetState createState() => _ParentWidgetState();
-}
-
-class _ParentWidgetState extends State<ParentWidget> {
-  bool _active = false;
-
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
-
-  Widget build(BuildContext context) {
-    return Container(
-      child: TapboxB(active: _active, onChanged: _handleTapboxChanged),
-    );
-  }
-}
-
-class TapboxB extends StatelessWidget {
-  TapboxB({Key key, this.active: false, @required this.onChanged})
-      : super(key: key);
-
-  final bool active;
-  final ValueChanged<bool> onChanged;
-
-  void _handleTap() {
-    onChanged(!active);
-  }
-
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _handleTap,
-      child: Container(
-        child: Center(
-          child: Text(
-            active ? "Active" : "Inactive",
-            style: TextStyle(fontSize: 32, color: Colors.white),
-          ),
-        ),
-        width: 200,
-        height: 200,
-        decoration:
-            BoxDecoration(color: active ? Colors.lightGreen : Colors.grey),
-      ),
-    );
-  }
-}
-
-class _TapboxAState extends State<TapboxA> {
-  bool _active = false;
-
-  void _handleTap() {
-    setState(() {
-      _active = !_active;
-    });
-  }
-
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _handleTap,
-      child: Container(
-        child: Center(
-          child: Text(
-            _active ? 'Active' : 'Inactive',
-            style: TextStyle(fontSize: 32, color: Colors.white),
-          ),
-        ),
-        width: 200,
-        height: 200,
-        decoration:
-            BoxDecoration(color: _active ? Colors.lightGreen : Colors.grey),
-      ),
-    );
-  }
-}
-
-class TapboxA extends StatefulWidget {
-  TapboxA({Key key}) : super(key: key);
-  _TapboxAState createState() => _TapboxAState();
 }
