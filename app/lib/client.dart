76b82925c6fe7ba19139cf7f416d0226528a4d5a
yama-koo <yamako.ilc@gmail.com>
2019-12-08 23:32:30 +0900

    http request

---
+++ app/lib/client.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<Post> fetchPost() async {
+  final response =
+      await http.get('https://jsonplaceholder.typicode.com/posts/1');
+
+  if (response.statusCode == 200) {
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load post');
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+class Photo {
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  Photo({this.albumId, this.id, this.title, this.url, this.thumbnailUrl});
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      thumbnailUrl: json['thumbnailUrl'] as String,
+    );
+  }
+}
+
+Future<List<Photo>> fetchPhotos(http.Client client) async {
+  final response =
+      await client.get('https://jsonplaceholder.typicode.com/photos');
+
+  // return parsePhotos(response.body);
+  return compute(parsePhotos, response.body);
+}
+
+List<Photo> parsePhotos(String responseBody) {
+  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
+}
+
+class PhotoList extends StatelessWidget {
+  final List<Photo> photos;
+
+  PhotoList({Key key, this.photos}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: photos.length,
+      itemBuilder: (context, index) {
+        return Image.network(photos[index].thumbnailUrl);
+      },
+    );
+  }
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,63 +1,56 @@
 // import 'package:app/routes.dart';
 // import 'package:app/screens.dart';
-import 'package:app/todo.dart';
+import 'package:app/client.dart';
+// import 'package:app/todo.dart';
 import 'package:flutter/material.dart';
 // import 'bottom_bar.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  final List listItem = ['西野千明', '小野寺梓', '橋本美桜', '麦田ひかる', '三浦菜々子', '鈴木えま'];
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<Post> post;
+
+  @override
+  void initState() {
+    super.initState();
+    // post = fetchPost();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout demo',
-      // initialRoute: '/',
-      // routes: {
-      // '/': (context) => FirstScreen(),
-      // '/second': (context) => SecondScreen(),
-      //   ExtractArgumentsScreen.routeName: (context) => ExtractArgumentsScreen(),
-      // },
-      // onGenerateRoute: (settings) {
-      //   if (settings.name == PassArgumentsScreen.routeName) {
-      //     final ScreenArguments args = settings.arguments;
-      //     return MaterialPageRoute(
-      //       builder: (context) {
-      //         return PassArgumentsScreen(
-      //           title: args.title,
-      //           message: args.message,
-      //         );
-      //       },
-      //     );
-      //   }
-      // },
-      home: TodosScreen(
-        todos: List<Todo>.generate(
-          20,
-          (i) => Todo(
-            'Todo $i',
-            'A description of what needs to be done for Todo $i',
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<List<Photo>>(
+            future: fetchPhotos(http.Client()),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) print(snapshot.error);
+              return snapshot.hasData
+                  ? PhotoList(
+                      photos: snapshot.data,
+                    )
+                  : Center(
+                      child: CircularProgressIndicator(),
+                    );
+            },
           ),
         ),
       ),
-      // home: FirstRoute(),
-      // Scaffold(
-      //   appBar: AppBar(
-      //     title: Text('真っ白なキャンパス'),
-      //   ),
-      //   body: Container(
-      //     child: ListView.builder(
-      //       itemBuilder: (BuildContext context, int index) {
-      //         return ListTile(
-      //           title: Text(listItem[index % 6]),
-      //           key: UniqueKey(),
-      //         );
-      //       },
-      //       itemCount: 200,
-      //     ),
-      //   ),
-      //   bottomNavigationBar: BottomBar(),
-      // ),
     );
   }
 }
